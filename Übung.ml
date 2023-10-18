43df00a74c10aab53dd36bc6626423c171f9561f
monnerjahnl0 <s6lumonn@uni-bonn.de>
2023-10-19 01:13:40 +0200

    Exercise

---
+++ Übung.ml
@@ -0,0 +1,87 @@
+(* If you want to use the code in the Terminal you need 
+    #use "Übung_src.ml"
+  and:
+    #use "Übungc.ml"
+*)
+#use "Übung_src.ml";; 
+
+
+(*
+  Exercise 1: Complete the following function to parse simple arithmetic expressions 
+  with Subtraction. The parser and printer should be able to handle expressions like:
+  (3 - 4) * 5 + 6 * 7
+  (3 - 4 + 5) * 6 + 7
+*)
+
+type exp = 
+    Var of string
+  | Const of int
+  | Add of exp * exp
+  | Sub of exp * exp (*Subtraction*)
+  | Mul of exp * exp;;
+
+let rec parse_expression i =
+  match parse_su i with
+    e1,"+"::i1 -> let e2,i2 = parse_expression i1 in Add(e1,e2),i2
+  | e1,i1 -> e1,i1
+
+  and parse_subtraction i = (* Add code for Parsing Subtraction*)
+
+  and parse_product i =
+    match parse_atom i with
+      e1,"*"::i1 -> let e2,i2 = parse_product i1 in Mul(e1,e2),i2
+    | e1,i1 -> e1,i1
+
+  and parse_atom i =
+    match i with
+      [] -> failwith "Expected an expression at end of input"
+    | "("::i1 -> (match parse_expression i1 with
+                    e2,")"::i2 -> e2,i2
+                  | _ -> failwith "Expected closing bracket")
+    | tok::i1 -> if List.for_all numeric (explode tok)
+                  then Const(int_of_string tok),i1
+                  else Var(tok),i1;;
+
+
+let default_parser = make_parser parse_expression;;
+
+let rec string_of_exp pr e =
+  match e with
+    Var s -> s
+  | Const n -> string_of_int n
+  | Add(e1,e2) ->
+        let s = (string_of_exp 3 e1)^" + "^(string_of_exp 2 e2) in
+        if 2 < pr then "("^s^")" else s
+  | Sub (* Add code for Printing Subtraction*)
+  | Mul(e1,e2) ->
+        let s = (string_of_exp 7 e1)^" * "^(string_of_exp 6 e2) in
+        if 6 < pr then "("^s^")" else s;;
+
+
+(*Gives out printet input*)
+let print_exp e = Format.print_string ("<<"^string_of_exp 0 e^">>");;
+
+(* To test you Function*)
+let e1 = default_parser "(3 - 4) * 5 + 6 * 7";;
+let e2 = default_parser "(3 - 4 + 5) * 6 + 7";;
+
+print_exp e1;;
+print_exp e2;;
+
+(*
+  Exercise 2: Extend the simplfy function to handle Subtraction in cases like:
+  x - x = 0
+  x - 0 = x
+  x -- y = x + y
+  x --- y = x - y 
+*)
+(*Tipp:   Therfor you need to edit the type expression and the function simplify*)
+
+
+
+
+(*
+   Extra Exercise: Modify the parser to handle unary minus. 
+   The parser and printer should be able to handle expressions like:
+    x --- x
+*)
---
+++ Übung_src.ml
@@ -0,0 +1,74 @@
+(*Splits stings into a list of chars
+  Input: Str
+  Output: Char List whit all charkters of the string*)
+let explode str =
+  let rec exp a b =
+    if a < 0 then b
+    else exp (a - 1) (str.[a] :: b)
+  in
+  exp (String.length str - 1) [];;
+
+
+(*Splits stings into a list of singel charater Strings
+  Input: Str
+  Output: Str List whit all charkters of the string*)
+let explode_str str = 
+  let rec exp a b =
+    if a < 0 then b
+    else let a' = Char.escaped str.[a] in exp (a - 1) ( a' :: b)
+  in
+  exp (String.length str - 1) [];;
+
+
+(*Checks if a chars is in a string
+  Input: Str Char
+  Output: bool if char in string*)  
+let matches s = let chars = explode s in fun c -> List.mem c chars;;  
+
+
+(*TODO: Überartbeite handhabung '`' ISO-Latin1*)
+(*Checks if a 'char' is of type: space, punctuation, symbolic, numeric, alphanumeric
+  Input: char
+  Output: bool if char is type*)  
+let space = matches " \t\n\r"
+  and punctuation = matches "()[]{},"
+  and symbolic = matches "~‘!@#$%^&*-+=|\\:;<>.?/"
+  and numeric = matches "0123456789"
+  and alphanumeric = matches
+    "abcdefghijklmnopqrstuvwxyz_’ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";;
+
+
+(*TODO: Try to get rit of this mess*)
+(*Checks if the first symbol of a str is of type: space, punctuation, symbolic, numeric, alphanumeric
+  Input: str
+  Output: bool if char is type*)  
+let space_str inp = matches " \t\n\r" inp.[0]
+  and punctuation_str inp = matches "()[]{},;" inp.[0]
+  and symbolic_str inp = matches "~‘!@#$%^&*-+=|\\:;<>.?/" inp.[0]
+  and numeric_str inp = matches "0123456789" inp.[0]
+  and alphanumeric_str inp = matches
+    "abcdefghijklmnopqrstuvwxyz_’ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789" inp.[0];;
+
+
+(*split a string in in too the beginnig and rest. Wher beging is the longest substig of type*)
+let rec lexwhile prop inp =
+  match inp with
+    c::cs when prop c -> let tok,rest = lexwhile prop cs in c^tok,rest
+  | _ -> "",inp;;
+
+
+(*Splits input by type usung lexwhile*)
+let rec lex inp =
+  match snd(lexwhile space_str inp) with
+    [] -> []
+  | c::cs -> let prop = if alphanumeric_str(c) then alphanumeric_str
+                        else if symbolic_str(c) then symbolic_str
+                        else fun c -> false in
+              let toktl,rest = lexwhile prop cs in
+              (c^toktl)::lex rest;;
+
+              
+(*Builds a parser with an parser function and lex to split the strings*)
+let make_parser pfn s =
+  let expr,rest = pfn (lex(explode_str s)) in
+    if rest = [] then expr else failwith "Unparsed input";;
